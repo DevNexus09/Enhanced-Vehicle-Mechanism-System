cfb1363359e7b3d76f9a8c757ab9af680a7735d8
Ashish Roy Shayon <ashish@Ashishs-MacBook-Air.local>
2025-05-22 09:14:53 +0600

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Car {
+
+    float currentFuelLevel;
+    float maxSpeed;
+
+    public void driveCar(){
+        if(currentFuelLevel<=0) System.out.println("Car is out of fuel");
+        else if(currentFuelLevel<=5){
+            System.out.println("Car is driving in reserve mode");
+            currentFuelLevel--;
+        }
+        else{
+            System.out.println("Car is driving");
+            currentFuelLevel--;
+        }
+    }
+
+    public void refuel(){
+        Scanner input = new Scanner(System.in);
+        float fuel = input.nextFloat();
+
+        currentFuelLevel+=fuel;
+        System.out.println("Car is refueled");
+    }
+
+    public float getSpeed(){
+        Scanner input = new Scanner(System.in);
+        float distanceDriven = input.nextFloat();
+        float duration = input.nextFloat();
+
+        return distanceDriven/duration;
+
+    }
+
+
+
+}
---
+++ src/Driver.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Driver {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+
+        myCar.currentFuelLevel = 2;
+
+        while(true) {
+            Scanner input = new Scanner(System.in);
+            int option = input.nextInt();
+            if(option==4) break;
+            switch(option) {
+                case 1:
+                    myCar.driveCar();
+                    break;
+                case 2:
+                    myCar.refuel();
+                    break;
+                case 3:
+                    float speed = myCar.getSpeed();
+                    System.out.println("Car Current Speed is: "+speed);
+                    break;
+            }
+        }
+        }
+}
